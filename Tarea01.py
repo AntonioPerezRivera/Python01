1de2e1225b07467d731512fd23ba7a4d03e60adc
CincoV <antoniom.p.rivera@gmail.com>
2015-03-10 20:26:00 +0100

    Añadido: '99 Bottles of beer'

---
+++ Tarea01.py
@@ -0,0 +1,13 @@
+
+""" 
+
+	Ejercicio 1 (Tarea 01) para la clase del dia 10/03/2015 de Sistemas Distribuidos
+	
+	Autores: Antonio Miguel Pérez Rivera - Rubén Rivas Troncoso
+
+"""
+
+for i in reversed(range(1,99)):
+
+    print "%s bottles of beer on the wall, %s bottles of beer.\nTake one down, pass it around, %s bottles of beer on the wall.\n" % (i,i,i-1)
+
